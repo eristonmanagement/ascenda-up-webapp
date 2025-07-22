5954264e479ecedd1ad3b0fe7639b0ebb6203c7a
Eriston Pedro <contato@eristonmanagement.com>
2025-07-22 18:42:33 -0300

    Remove uso de DOC_ID da integração com DocsBot

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require("express");
 const { Telegraf } = require("telegraf");
 const fetch = require("node-fetch");
+const express = require("express");
 
-const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const app = express();
 
 app.use(express.json());
 
 // Mensagem de boas-vindas
-bot.start((ctx) => {
-  ctx.reply("Olá, Sou o Ascenda Up, seu mentor no Telegram. Vou te ajudar a dar um Up na carreira!");
-});
+bot.start((ctx) =>
+  ctx.reply("Olá, sou o Ascenda Up, seu mentor no Telegram. Vou te ajudar a dar um Up na carreira!")
+);
 
-// Encaminhar mensagens para a API do DocsBot
+// Encaminhar mensagens para o DocsBot
 bot.on("text", async (ctx) => {
   const userMsg = ctx.message.text;
 
   try {
-    const response = await fetch(`https://app.docsbot.ai/api/ask/${process.env.DOCSBOT_BOT_ID}/${process.env.DOCSBOT_DOC_ID}`,
+    const response = await fetch(
+      `https://app.docsbot.ai/api/ask/${process.env.DOCSBOT_BOT_ID}`, // sem Doc ID
       {
         method: "POST",
         headers: {
@@ -32,18 +33,17 @@ bot.on("text", async (ctx) => {
     );
 
     const data = await response.json();
-    ctx.reply(data.answer || "Não encontrei uma resposta no momento.");
+    ctx.reply(data.answer || "Não consegui encontrar uma resposta no momento.");
   } catch (err) {
     console.error(err);
     ctx.reply("Ocorreu um erro. Tente novamente.");
   }
 });
 
-// Webhook
+// Webhook e porta
 app.use(bot.webhookCallback("/bot"));
 bot.telegram.setWebhook(`${process.env.RENDER_EXTERNAL_URL}/bot`);
 
-// Inicializa servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor do Ascenda Mentor ativo na porta ${PORT}`);
