8d5f3f8c89a983e2bb8c1c3ea440735cccdf1ece
Eriston Pedro <contato@eristonmanagement.com>
2025-07-23 19:04:42 -0300

    fix: exporta auth corretamente no firebase.js

--- ascenda-up-webapp/public/firebase.js
+++ ascenda-up-webapp/public/firebase.js
@@ -1,8 +1,12 @@
 // public/firebase.js
 
-// Importa e inicializa o Firebase
+// Importa funções necessárias
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+// Configurações do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBkgWXJNrLj0ur-bliwxdDoQrikrdCfL7U",
+  apiKey: "AIzaSyBkgWXJNrLrj0ur-bliwxdDoQrikrdCfL7U",
   authDomain: "ascendaup.firebaseapp.com",
   projectId: "ascendaup",
   storageBucket: "ascendaup.appspot.com",
@@ -11,4 +15,9 @@ const firebaseConfig = {
   measurementId: "G-NN8HW8JY8D"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Exporta o auth para ser usado no index.html
+export { auth };
