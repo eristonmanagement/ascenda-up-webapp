ce4cff1e3e50904983d3bc076a48b4db52219abd
Eriston Pedro <contato@eristonmanagement.com>
2025-07-23 19:02:14 -0300

    fix: corrige firebase.js com apiKey válida do Firebase

--- ascenda-up-webapp/public/firebase.js
+++ ascenda-up-webapp/public/firebase.js
@@ -1,16 +1,14 @@
 // public/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
 
+// Importa e inicializa o Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBgk9WkJNLrj0ur-bliwxdDoQrikrdCfL7U",
+  apiKey: "AIzaSyBkgWXJNrLj0ur-bliwxdDoQrikrdCfL7U",
   authDomain: "ascendaup.firebaseapp.com",
   projectId: "ascendaup",
   storageBucket: "ascendaup.appspot.com",
-  messagingSenderId: "934093801271",
-  appId: "1:934093801271:web:59536a45f3ed67ce35f2ea",
+  messagingSenderId: "934093010271",
+  appId: "1:934093010271:web:59536a45f3ed67ce35f2ea",
   measurementId: "G-NN8HW8JY8D"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+firebase.initializeApp(firebaseConfig);
