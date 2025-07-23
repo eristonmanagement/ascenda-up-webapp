72578a1a7c589308fd2c6d7df428a5a9b989aab8
Eriston Pedro <contato@eristonmanagement.com>
2025-07-23 20:02:52 -0300

    Corrige storageBucket na configuração do Firebase

--- ascenda-up-webapp/public/firebase.js
+++ ascenda-up-webapp/public/firebase.js
@@ -1,16 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDGE8J19XDGF7kvx6LZohh1vZgTNJqpLCA",
   authDomain: "ascendaup-app.firebaseapp.com",
   projectId: "ascendaup-app",
-  storageBucket: "ascendaup-app.firebasestorage.app",
+  storageBucket: "ascendaup-app.appspot.com", // corrigido aqui
   messagingSenderId: "278146844694",
   appId: "1:278146844694:web:95395cb36eef08d11a8d37",
   measurementId: "G-24YVW5SCFH"
